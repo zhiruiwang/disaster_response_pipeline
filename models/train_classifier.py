1f772e454eccaaa82d36d5c05794a6b1136b9d55
zhiruiwang <zw2389@columbia.edu>
2019-06-18 23:56:28 -0400

    Update train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -97,7 +97,7 @@ def build_model():
     # Set up the search grid
     parameters = {
         'features__text_pipeline__tfidf__use_idf': (True, False),
-        'clf__estimator__n_estimators': [50, 100, 200]
+        'clf__estimator__n_estimators': [50, 100]
     }
     # Initialize GridSearch cross validation object
     cv = GridSearchCV(pipeline, param_grid=parameters,n_jobs=-1)
